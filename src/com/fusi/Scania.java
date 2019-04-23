2bf909a233c825c24ed870db7c4a8fa554af36b9
windows <windows@LAPTOP-8C6908TI>
2019-04-23 17:12:22 +0700

    Done

---
+++ src/com/fusi/Scania.java
@@ -0,0 +1,16 @@
+package com.fusi;
+
+public class Scania {
+	
+	int x;
+	
+	public Scania() {
+		x=3;
+	}
+	public static void main(String[] args) {
+		Scania y = new Scania();
+		System.out.println(y.x);
+
+	}
+
+}
