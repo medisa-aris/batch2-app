401c30683abc56045314468a329da930bb7c5f1f
HP <HP@user>
2019-04-23 17:09:37 +0700

    Updated

---
+++ src/com/fusi/Truck.java
@@ -0,0 +1,16 @@
+package com.fusi;
+
+public class Truck extends Main {
+	public Truck(){
+		
+	}
+	public void drive() {
+		
+	}
+	public void street(int i) {
+		
+	}
+	public void stop() {
+		
+	}
+}
